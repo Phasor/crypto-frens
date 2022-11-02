a0ad28582cd0fd54e489fe922ffc216a0301d9a5
ben <phas0ruk@gmail.com>
2022-11-01 17:17:28 -0700

    cleans up routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,25 +4,7 @@ const authController = require('../controllers/authController');
 const passport = require('passport');
 
 router.post('/login', authController.login);
-router.get('/login/failed', (req, res) => {res.status(401).json({success: false, message: 'Login failed'})});
-router.get('/login/success', (req, res) => {
-    if(req.user){
-        res.status(200).json({success: true, message: 'Login successful', user: req.user, cookies: req.cookies})
-    } else {
-        res.json({success: false, message: 'Login failed'})
-    }
-
-});
-// router.get('/facebook', passport.authenticate('facebook', {scope: ['profile']}));
-// router.get('/facebook/callback', passport.authenticate('facebook', 
-//   {
-//     failureRedirect: '/login/failed',
-//     successRedirect: 'http://localhost:3002/home' // client side redirect
-// }));
-
-
 router.get('/google/callback', passport.authenticate('google', {session: false}), authController.googleAuthCallback);
 router.get('/google', passport.authenticate('google', {session: false, scope: ['email','profile']}) );
-
 
 module.exports = router;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,52 +6,29 @@ import { Link, useLocation } from 'react-router-dom';
 export default function Home() {
     const username = localStorage.getItem('username');
     const [tokenGoog, setTokenGoog] = useState('');
-    // const [googUser, setGoogUser] = useState(null);
     const [firstName, setFirstName] = useState('');
+    const [ready, setReady] = useState(false);
     const location = useLocation();
 
     useEffect(() => {
         const setDataFromUrl = async () => {
-            if(location.search){ // there is a query string, hence this is a Google auth login   
+            if(location.search){ // there is a query string, hence this is a Google auth login, not a local auth login
                 const rawData = location.search.split("=")[1];
-                const removedFirstName = rawData.split("&")[0];
-                const tokenValue =  removedFirstName.replace("%20", " ");
+                const firstElem = rawData.split("&")[0];
+                const tokenValue =  firstElem.replace("%20", " ");
                 // console.log(`tokenValue: ${tokenValue}`);
                 setTokenGoog(tokenValue);
                 localStorage.setItem("token", tokenGoog);
-                const firstNameValue = location.search.split("=")[2];
+                const firstNameRaw = location.search.split("=")[2];
+                const firstNameValue = firstNameRaw.split("&")[0];
                 setFirstName(firstNameValue);
                 const userIDValue = location.search.split("=")[3];
                 localStorage.setItem("userID", userIDValue);
+                setReady(true);
             }
         }
         setDataFromUrl();
     }, [tokenGoog, location.search]);
-
-
-    // // get user from google oauth if required
-    // useEffect(() => {
-    //     const getUser = async () => {
-    //         const response = await fetch("http://localhost:3000/api/v1/auth/login/success", {
-    //             method: "GET",
-    //             credentials: "include",
-    //             headers: {
-    //             Accept: "application/json",
-    //             "Content-Type": "application/json",
-    //             "Access-Control-Allow-Credentials": true,
-    //             },
-    //         })
-    //         if (response.status === 200){
-    //             const fetchedUser = await response.json();
-    //             console.log('user', fetchedUser);
-    //             setGoogUser(fetchedUser);
-    //         } 
-    //         else{
-    //             throw new Error("authentication has been failed!");
-    //         }
-    //     };
-    //     getUser();
-    //   }, []);
 
   return (
     <div>
@@ -61,7 +38,7 @@ export default function Home() {
                 <h1>Home</h1>
                 {firstName ? <p>Welcome {firstName}</p> : <p>Welcome {username}</p>}
                 <Link to="/post/create"><button>Create Post</button></Link>
-                <PostList/>
+                <PostList/> 
             </div>
         </div>
     </div>
