21e32cc18810ac2e48aa30652c6f53aae4cc08e1
ben <phas0ruk@gmail.com>
2022-11-21 16:29:05 -0800

    styles lightbox for modal

--- frontend/src/components/ImageModal.js
+++ frontend/src/components/ImageModal.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function ImageModal({ postURL, setShowModal, showModal }) {
 
   return (
-    <div className='flex items-center justify-center p-4 rounded bg-white'>
-        <div className='p-2'>
-            <img src={postURL} className=" max-w-[1000px] max-h-[800px] object-cover cursor-pointer" alt="" onClick={() => setShowModal(!showModal)}/>
-            <p className='text-yellow-800 text-5xl absolute top-0 right-0 p-2 z-80'>HELLLOLO</p>
+    <div className='flex items-center justify-center p-4 rounded bg-white '>
+        <div className='p-10 bg-gray-800 rounded-md relative'>
+            <img src={postURL} className=" max-w-[1000px] max-h-[1000px] object-cover cursor-pointer" alt="" onClick={() => setShowModal(!showModal)}/>
+            <div className='absolute top-0 right-0 p-1'>
+                <XMarkIcon className='w-10 h-10 cursor-pointer text-white' onClick={() => setShowModal(!showModal)} />
+            </div>
         </div>
     </div>
   )
