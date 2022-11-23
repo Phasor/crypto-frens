d697cb62c0a229981be53f62bd843dab1e3f1c94
ben <phas0ruk@gmail.com>
2022-11-23 13:49:05 -0800

    fixes missing prop setDataFromURL() into FriendsList.js

--- frontend/src/pages/MyPosts.js
+++ frontend/src/pages/MyPosts.js
@@ -5,32 +5,32 @@ import MyPostList from "../components/MyPostList";
 import { useLocation } from "react-router-dom";
 
 export default function MyPosts() {
-  const location = useLocation();
+    const location = useLocation();
 
-  const setDataFromUrl = async () => {
-    if (location.search) {
-      // there is a query string, hence this is a Google auth login, not a local auth login
-      const rawData = location.search.split("=")[1];
-      const firstElem = rawData.split("&")[0];
-      const tokenValue = firstElem.replace("%20", " ");
-      // console.log(`tokenValue: ${tokenValue}`);
-      // setTokenGoog(tokenValue);
-      localStorage.setItem("token", tokenValue);
-      const userIDValue = location.search.split("=")[3];
-      localStorage.setItem("userID", userIDValue);
-    }
-  };
+    const setDataFromUrl = async () => {
+        if (location.search) {
+            // there is a query string, hence this is a Google auth login, not a local auth login
+            const rawData = location.search.split("=")[1];
+            const firstElem = rawData.split("&")[0];
+            const tokenValue = firstElem.replace("%20", " ");
+            // console.log(`tokenValue: ${tokenValue}`);
+            // setTokenGoog(tokenValue);
+            localStorage.setItem("token", tokenValue);
+            const userIDValue = location.search.split("=")[3];
+            localStorage.setItem("userID", userIDValue);
+        }
+    };
 
-  return (
-    <div>
-      <div>
-        <NavBar setDataFromUrl={setDataFromUrl} />
-        <div className="w-full h-screen flex justify-center">
-          <Sidebar setDataFromUrl={setDataFromUrl} />
-          <MyPostList />
-          <FriendsList />
+    return (
+        <div>
+            <div>
+                <NavBar setDataFromUrl={setDataFromUrl} />
+                <div className="w-full h-screen flex justify-center">
+                    <Sidebar setDataFromUrl={setDataFromUrl} />
+                    <MyPostList />
+                    <FriendsList setDataFromUrl={setDataFromUrl} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
